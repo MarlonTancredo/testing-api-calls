43cf158c1a16ca25cef0e7bc2fad9e93dd1530bd
unknown <marlon_tancredo@hotmail.com>
2023-09-20 15:57:40 +0100

    feat: implementing card.test.tsx

---
+++ src/components/Card.test.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Card from "./Card";
+import useFetchPersonName from './useFetchPerson';
+
+const useFetchPersonNameMock = useFetchPersonName as jest.Mock;
+
+jest.mock('./useFetchPerson');
+
+describe("Card Test", () => {
+    it("should displays Fetching when the data is undefined", () => {
+        render(<Card />);
+
+        expect(screen.getByText(/Fetching.../i)).toBeInTheDocument();
+    });
+
+    it("should displays Fetching when the data is undefined", () => {
+        useFetchPersonNameMock.mockReturnValue('Name: Luke Skywalker');
+
+        render(<Card />);
+
+        expect(screen.getByText(/Name: Luke Skywalker/i)).toBeInTheDocument();
+    });
+})
